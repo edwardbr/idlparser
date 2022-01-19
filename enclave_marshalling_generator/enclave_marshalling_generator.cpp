09807cbbf4c2b43476f6fcbcd7949a2cb4cbe562
Edward Boggis-Rolfe <edward@secretarium.com>
2022-01-19 16:12:09 +0000

    Initial cpp_generation candidate

--- enclave_marshalling_generator/enclave_marshalling_generator.cpp
+++ enclave_marshalling_generator/enclave_marshalling_generator.cpp
@@ -57,15 +57,35 @@ namespace enclave_marshalling_generator
 {
     namespace host_ecall
     {
-        void write_interface(const ClassObject& m_ob, writer& header, writer& source)
+        void write_interface_predeclaration(const Library& lib, const ClassObject& m_ob, writer& header, writer& source)
+        {
+            source("class {}_proxy;", 
+                m_ob.name, 
+                m_ob.name);
+        };
+
+        void write_interface(const Library& lib, const ClassObject& m_ob, writer& header, writer& source)
         {
             header("class {}{}{} : public i_unknown", 
                 m_ob.name, 
                 m_ob.parentName.empty() ? "" : ":",
                 m_ob.parentName);
 			header("{{");
-			source("{{");
 			header("public:");
+
+            source("class {}_proxy : public {}", 
+                m_ob.name, 
+                m_ob.name);
+			source("{{");
+            source("xxxxx& xxxx_;");
+            source("uint64_t object_id_;");
+			source("public:");
+			source("");
+            source("{}_proxy(xxxxx& xxxx, uint64_t object_id) : ", m_ob.name);
+            source("  xxxx_(xxxx),", m_ob.name);
+            source("  object_id_(object_id)", m_ob.name);
+            source("  {{}}", m_ob.name);
+			source("");
 
             for(auto& function : m_ob.functions)
             {
@@ -75,7 +95,7 @@ namespace enclave_marshalling_generator
                 header.print_tabs();
                 source.print_tabs();
                 header.raw("virtual {} {}(", function.returnType, function.name);
-                source.raw("virtual {} {}(", function.returnType, function.name);
+                source.raw("virtual {} {}_proxy::{} override (", function.returnType, m_ob.name, function.name);
                 bool has_parameter = false;
                 for(auto& parameter : function.parameters)
                 {
@@ -91,12 +111,56 @@ namespace enclave_marshalling_generator
                 header.raw(") = 0;\n");
                 source.raw(")\n");
                 source("{{");
+
+                source("const auto in_ =  = yas::save<yas::mem|yas::json>(YAS_OBJECT_NVP(");
+                source("  \"in\"");
+
+                int count = 0;
+                for(auto& parameter : function.parameters)
+                {
+                    auto in_it = std::find(parameter.m_attributes.begin(), parameter.m_attributes.end(), "in");
+                    auto out_it = std::find(parameter.m_attributes.begin(), parameter.m_attributes.end(), "out");
+                    auto byval_it = std::find(parameter.m_attributes.begin(), parameter.m_attributes.end(), "by_value");
+                    if(out_it != parameter.m_attributes.end() && in_it == parameter.m_attributes.end())
+                        continue;
+
+                    source("  ,(\"_{}\", {})", count++, parameter.name);
+                }
+                
+                source("  ));");
+                
+                source("std::vector<uint8_t> out_;");
+                source("int ret = call_stub(in_, out_);");
+                source("if(ret)");
+                source("{{");
+                source("return ret;");
                 source("}}");
+
+                source("yas::load<yas::mem|yas::json>(out_, YAS_OBJECT_NVP(");
+                source("  \"out\"");
+
+                count = 0;
+                for(auto& parameter : function.parameters)
+                {
+                    auto in_it = std::find(parameter.m_attributes.begin(), parameter.m_attributes.end(), "in");
+                    auto out_it = std::find(parameter.m_attributes.begin(), parameter.m_attributes.end(), "out");
+                    if(out_it == parameter.m_attributes.end() && in_it != parameter.m_attributes.end())
+                        continue;
+
+                    source("  ,(\"_{}\", {})", count++, parameter.name);
+                }
+                source("  ));");
+
+                source("return ret;");
+                source("}}");
+                source("");
             }
 
-            header("}};");
-            source("}}");
+            header("}};");            
             header("");
+
+            source("}};");            
+            source("");
         };
 
         void write_struct(const ClassObject& m_ob, writer& header, writer& source)
@@ -106,47 +170,50 @@ namespace enclave_marshalling_generator
                 m_ob.parentName.empty() ? "" : ":",
                 m_ob.parentName);
 			header("{{");
-			source("{{");
 
-            for(auto& function : m_ob.functions)
+            for(auto& field : m_ob.functions)
             {
-                if(function.type != FunctionTypeVariable)
+                if(field.type != FunctionTypeVariable)
                     continue;
                 
                 header.print_tabs();
-                header.raw("{} {};\n", function.returnType, function.name);
-                source("{{");
-                source("}}");
+                header.raw("{} {};\n", field.returnType, field.name);
             }
 
+            header("");
+            header("// one member-function for save/load");
+            header("template<typename Ar>");
+            header("void serialize(Ar &ar)");
+            header("{{");
+            header("ar & YAS_OBJECT(\"{}\"",m_ob.name);
+
+            int count = 0;
+            for(auto& field : m_ob.functions)
+            {
+                header("  ,(\"_{}\", {})", count++, field.name);
+            }
+            header(")");
+
+            header("}}");
 			
 
             header("}};");
-            source("}}");
         };
 
         void write_library(const Library& lib, const ClassObject& m_ob, writer& header, writer& source)
         {
-            header("//a placeholder class");
-            header("class i_unknown");
-            header("{{");
-            header("}};");
-            header("");
-
-            header("//a handler for new threads, this function needs to be thread safe!");
-            header("class i_thread_target : public i_unknown");
-            header("{{");
-            header("virtual error_code thread_started(std::string& thread_name) = 0;");
-            header("}};");
-            header("");
-
-            header("//a handler for new threads, this function needs to be thread safe!");
-            header("class i_message_target : public i_unknown");
-            header("{{");
-            header("virtual error_code add_peer(std::string peer_name, i_message_channel& channel) = 0;");
-            header("virtual error_code remove_queue_pair(std::string peer_name) = 0;");
-            header("}};");
-            header("");
+            for(auto& name : m_ob.m_ownedClasses)
+            {
+                const ClassObject* obj = nullptr;
+                if(!lib.FindClassObject(name, obj))
+                {
+                    continue;
+                }
+                if(obj->type == ObjectTypeInterface)
+                    write_interface_predeclaration(lib, *obj, header, source);
+            }
+            
+            source("");
 
             for(auto& name : m_ob.m_ownedClasses)
             {
@@ -156,24 +223,14 @@ namespace enclave_marshalling_generator
                     continue;
                 }
                 if(obj->type == ObjectTypeInterface)
-                    write_interface(*obj, header, source);
+                    write_interface(lib, *obj, header, source);
             }
 
-            header("//the class that encapsulates an environment or zone");
-            header("class {} : public i_zone", m_ob.name);
+
+            header("//a marshalable interface for other zones");
+            header("class i_{} : public i_zone", m_ob.name);
 			header("{{");
             header("public:");
-
-            header("{}(std::string& dll_file_name);", m_ob.name);
-            header("~{}();", m_ob.name);
-            header("");
-
-            header("//this runs until the thread dies, this will also setup a connection with the message pump");
-            header("void start_thread(std::string thread_name, i_thread_target& target);");
-            header("");
-
-            header("//this is to allow messaging between enclaves");
-            header("error_code create_message_link(std::string link_name, i_zone& other_zone, i_message_channel& channel);");
             header("");
 
             header("//polymorphic helper functions");
@@ -186,7 +243,7 @@ namespace enclave_marshalling_generator
                 }
                 if(obj->type == ObjectTypeInterface)
                 {
-                    header("error_code query_interface(i_unknown& from, std::shared_ptr<{0}>& to);", obj->name);
+                    header("virtual error_code query_interface(i_unknown& from, remote_shared_ptr<{0}>& to) = 0;", obj->name);
                 }
             }	
 
@@ -198,9 +255,70 @@ namespace enclave_marshalling_generator
                     continue;
                 
                 header.print_tabs();
+                header.raw("virtual {} {}(", function.returnType, function.name);
+                bool has_parameter = false;
+                for(auto& parameter : function.parameters)
+                {
+                    if(has_parameter)
+                    {
+                        header.raw(", ");
+                    }
+                    has_parameter = true;
+                    header.raw("{} {}",parameter.type, parameter.name);
+                }
+                header.raw(") = 0;\n");
+            }
+            
+            header("}};");
+            header("");
+            
+            
+            header("#ifndef _IN_ENCLAVE");
+            header("//the class that encapsulates an environment or zone");
+            header("//only host code can use this class directly other enclaves *may* have access to the i_zone derived interface");
+            header("class {} : public i_{}", m_ob.name, m_ob.name);
+			header("{{");
+            header("std::unique_ptr<enclave_info> enclave_;");
+            header("public:");
+
+            header("{}();", m_ob.name);
+            header("~{}();", m_ob.name);
+            header("");
+            header("error_code load(std::string& dll_file_name);");
+            header("");
+
+            source("error_code {}::load(std::string& dll_file_name);", m_ob.name);
+            source("{{");
+            source("}}");
+            source("");
+
+            header("//polymorphic helper functions");
+            for(auto& name : m_ob.m_ownedClasses)
+            {
+                const ClassObject* obj = nullptr;
+                if(!lib.FindClassObject(name, obj))
+                {
+                    continue;
+                }
+                if(obj->type == ObjectTypeInterface)
+                {
+                    header("error_code query_interface(i_unknown& from, remote_shared_ptr<{}>& to) override;", obj->name);
+                    source("error_code {}::query_interface(i_unknown& from, remote_shared_ptr<{}>& to) override;", obj->name, m_ob.name);
+                    source("{{");
+                    source("}}");
+                }
+            }	
+            header("");
+            header("//static functions passed to global functions in the target zone");
+            for(auto& function : m_ob.functions)
+            {
+                if(function.type != FunctionTypeMethod)
+                    continue;
+                
+                header.print_tabs();
                 source.print_tabs();
                 header.raw("{} {}(", function.returnType, function.name);
-                source.raw("{} {}(", function.returnType, function.name);
+                source.raw("{} {}::{}(", function.returnType, m_ob.name, function.name);
                 bool has_parameter = false;
                 for(auto& parameter : function.parameters)
                 {
@@ -213,13 +331,13 @@ namespace enclave_marshalling_generator
                     header.raw("{} {}",parameter.type, parameter.name);
                     source.raw("{} {}",parameter.type, parameter.name);
                 }
-                header.raw(");\n");
+                header.raw(") override;\n");
                 source.raw(")\n");
                 source("{{");
                 source("}}");
-            }
+            }            
             header("}};");
-            source("}};");
+            header("#endif //_IN_ENCLAVE");
         };
 
         //entry point
@@ -235,17 +353,64 @@ namespace enclave_marshalling_generator
             header("");
             header("using error_code = int;");
             header("");
-            header("//logical security environments");
-            header("class i_zone{{}};");
-            header("//a message channel between zones (a pair of spsc queues behind an executor)");
-            header("class i_message_channel{{}};");            
+
+            header("//a shared pointer that works accross enclaves");
+            header("template<class T>class remote_shared_ptr{{}};");
             header("");
+
+            header("//a weak pointer that works accross enclaves");
+            header("template<class T>class remote_weak_ptr{{}};");
+            header("");
+
+            header("class enclave_info;");
+            header("");            
+
+            header("//the base interface to all interfaces");
+            header("class i_unknown{{}};");
+            header("");
+
+            header("//a handler for new threads, this function needs to be thread safe!");
+            header("class i_thread_target : public i_unknown");
+            header("{{");
+            header("virtual error_code thread_started(std::string& thread_name) = 0;");
+            header("}};");
+            header("");
+
+            header("//a message channel between zones (a pair of spsc queues behind an executor) not thread safe!");
+            header("class i_message_channel : public i_unknown{{}};");            
+            header("");
+
+            header("//a handler for new threads, this function needs to be thread safe!");
+            header("class i_message_target : public i_unknown");
+            header("{{");
+            header("//Set up a link with another zone");
+            header("virtual error_code add_peer_channel(std::string link_name, i_message_channel& channel) = 0;");
+            header("//This will be called if the other zone goes down");
+            header("virtual error_code remove_peer_channel(std::string link_name) = 0;");
+            header("}};");
+            header("");
+
+            header("//logical security environment");
+            header("class i_zone : public i_unknown");
+            header("{{");
+            header("//this runs until the thread dies, this will also setup a connection with the message pump");
+            header("void start_thread(i_thread_target& target, std::string thread_name);");
+            header("");
+
+            header("//this is to allow messaging between enclaves this will create an i_message_channel");
+            header("error_code create_message_link(i_message_target& target, i_zone& other_zone, std::string link_name);");
+            header("}};");
+            header("");
+
+  			source("#include \"{}\"", header_filename);
+            source("");
+
 
             for(auto& ns : namespaces)
             {
                 header("namespace {}", ns);
                 header("{{");
-                source("namespace {}{{", ns);
+                source("namespace {}", ns);
                 source("{{");
             }
 
@@ -271,9 +436,6 @@ namespace enclave_marshalling_generator
                 if(obj->type == ObjectLibrary)
                     write_library(lib, *obj, header, source);
             }
-
-			source("#include \"{}\"", header_filename);
-
 
             for(auto& ns : namespaces)
             {
